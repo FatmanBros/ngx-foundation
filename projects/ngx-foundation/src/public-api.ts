72e8675747d08bcdecd6efc9ea06199d130853f3
takuma <takuma.watanabe@sola-air.com>
2021-06-03 08:41:06 +0900

    fix: add pipe to api file

--- projects/ngx-foundation/src/public-api.ts
+++ projects/ngx-foundation/src/public-api.ts
@@ -7,6 +7,9 @@ export * from './lib/ngx-foundation.component';
 export * from './lib/ngx-foundation.module';
 export * from './lib/mat.module';
 
+/**
+ * components
+ */
 export * from './lib/control/button/button.component';
 export * from './lib/control/label/label.component';
 export * from './lib/control/textbox/textbox.component';
@@ -17,10 +20,20 @@ export * from './lib/control/select/select.component';
 export * from './lib/control/datepicker/datepicker.component';
 export * from './lib/control/toolbar/toolbar.component';
 
+/**
+ * validators
+ */
 export * from './lib/validate/custom-validators';
+
+/**
+ * form control
+ */
 export * from './lib/control/custom-form-control';
 
+/**
+ * other
+ */
 export * from './lib/constants/constants';
 export * from './lib/enum/enums';
-
 export * from './lib/ngx-foundation.options';
+export * from './lib/pipe/number-with-commas.pipe';
