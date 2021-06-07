0c1a9849cedfe785aae7c31d47fc11b1a80878d8
takuma <takuma.watanabe@sola-air.com>
2021-06-07 23:15:21 +0900

    update: slider

--- projects/ngx-foundation/src/lib/control/contents-slider/contents-slider.component.ts
+++ projects/ngx-foundation/src/lib/control/contents-slider/contents-slider.component.ts
@@ -1,14 +1,78 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-contents-slider',
+  selector: 'foundation-contents-slider',
   templateUrl: './contents-slider.component.html',
   styleUrls: ['./contents-slider.component.scss'],
 })
 export class ContentsSliderComponent implements OnInit {
-  @Input() contentSize: { width: string; height?: string } = { width: '100%' };
+  @Input() width: string | number = '100%';
+  @Input() height: string | number = '30em';
+  @Input() contentWidth: number = 480;
 
-  constructor() {}
+  private isDragging: boolean = false;
+  private contentX: number = 0;
+  private startX: number = 0;
+  private moveX: number = 0;
+  private contentCnt: number = 0;
+
+  private currentNo: number = 0;
+
+  constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const div: Element =
+      this.elementRef.nativeElement.querySelector('div.inner');
+    this.contentCnt = div.children.length;
+    this.contentX = this.elementRef.nativeElement.offsetLeft;
+  }
+
+  public get outerStyles() {
+    return {
+      width: this.width,
+      height: this.height,
+    };
+  }
+
+  public get innerStyles() {
+    return {
+      'transition-duration': this.isDragging ? '0s' : '.5s',
+    };
+  }
+
+  public dragStart(event: MouseEvent) {
+    this.isDragging = true;
+    this.startX = event.screenX;
+  }
+
+  public dragEnd(event: MouseEvent) {
+    this.isDragging = false;
+    // calc current no
+    const diff = Math.round(-this.moveX / this.contentWidth);
+    this.currentNo += diff;
+    if (this.currentNo < 0) {
+      this.currentNo = 0;
+    }
+    if (this.currentNo > this.contentCnt - 1) {
+      this.currentNo = this.contentCnt - 1;
+    }
+    this.moveX = 0;
+  }
+
+  public drag(event: MouseEvent) {
+    if (!this.isDragging) {
+      return;
+    }
+    this.moveX = event.screenX - this.startX;
+  }
+
+  public get left() {
+    if (this.isDragging) {
+      return -(this.currentNo * this.contentWidth) + this.moveX + 'px';
+    } else {
+      return -(this.currentNo * this.contentWidth) + 'px';
+    }
+  }
 }
